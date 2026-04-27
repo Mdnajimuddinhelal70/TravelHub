871e47371af345146e9fe537d0e40220a543f0fd
unknown <najimuddinhelalal670@gmail.com>
2026-04-27 19:55:34 +0100

    added tour related apis

--- backend/src/app/router/index.ts
+++ backend/src/app/router/index.ts
@@ -1,12 +1,22 @@
 import { Router } from "express";
+import { AuthRoutes } from "../modules/auth/auth.route";
+import { TourRoutes } from "../modules/tour/tour.route";
 import { UserRoutes } from "../modules/user/user.route";
 
 export const router = Router();
 
 const moduleRoutes = [
   {
+    path: "/auth",
+    route: AuthRoutes,
+  },
+  {
     path: "/user",
     route: UserRoutes,
+  },
+  {
+    path: "/tour",
+    route: TourRoutes,
   },
 ];
 
